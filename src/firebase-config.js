9f4729991a0a1488b7666c1f78da45141db73c1e
Agustin Escoda <32514472+Anform@users.noreply.github.com>
2022-07-18 19:13:43 -0400

    Update firebase-config.js
    Removed sensitive information about firebase database

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,13 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "@firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyADy0ejB-f0w2rXr9uoKQiR1Ld8NI3wJqg",
-  authDomain: "task-list-6ff79.firebaseapp.com",
-  projectId: "task-list-6ff79",
-  storageBucket: "task-list-6ff79.appspot.com",
-  messagingSenderId: "331518723650",
-  appId: "1:331518723650:web:379f39c7bce85f80b1cfdb",
-  measurementId: "G-P8VSDRL4LP"
+  //Go to https://firebase.google.com/ to get firebase config
 };
 
 // Initialize Firebase
